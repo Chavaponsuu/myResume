dcb025e97039ea72aa53f99d62571990a3c9eb79
Chavaponsuu <chavapon1402@gmail.com>
2025-07-05 19:26:05 +0700

    myskills section

--- scripts.js
+++ scripts.js
@@ -3,7 +3,12 @@ document.querySelector(".hamburger").addEventListener("click" , function(){
     document.querySelector(".menu").classList.toggle("show");
 })
 
-
+// document.getElementById("myskills").addEventListener("click", function() {
+//     const skillsSection = document.getElementById("skills");
+//     if (skillsSection) {
+//         skillsSection.scrollIntoView({ behavior: "smooth" });
+//     }
+// });
 
 
 
